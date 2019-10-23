e20e34ea42c9b7627cca805744d792b0909a8f1a
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-23 21:53:16 +0700

    add editEmployee function

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
     <employee-form @add:employee="addEmployee" />
-    <employee-table v-bind:employees="employees" @delete:employee="deleteEmployee" />
+    <employee-table
+      v-bind:employees="employees"
+      @delete:employee="deleteEmployee"
+      @edit:employee="editEmployee"
+    />
   </div>
 </template>
 
@@ -57,6 +61,12 @@ export default {
 
     deleteEmployee(id) {
       this.employees = this.employees.filter(employee => employee.id !== id);
+    },
+
+    editEmployee(id, updatedEmployee) {
+      this.employees = this.employees.map(employee =>
+        employee.id === id ? updatedEmployee : employee
+      );
     }
   }
 };
--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -10,10 +10,20 @@
       </thead>
       <tbody>
         <tr v-for="employee in employees" :key="employee.id">
-          <td>{{ employee.name }}</td>
-          <td>{{ employee.email }}</td>
-          <td>
-            <button>Edit</button>
+          <td v-if="editing === employee.id">
+            <input type="text" v-model="employee.name" />
+          </td>
+          <td v-else>{{employee.name}}</td>
+          <td v-if="editing === employee.id">
+            <input type="text" v-model="employee.email" />
+          </td>
+          <td v-else>{{employee.email}}</td>
+          <td v-if="editing === employee.id">
+            <button @click="editEmployee(employee)">Save</button>
+            <button class="muted-button" @click="cancelEdit(employee)">Cancel</button>
+          </td>
+          <td v-else>
+            <button @click="editMode(employee.id)">Edit</button>
             <button @click="$emit('delete:employee', employee.id)">Delete</button>
           </td>
         </tr>
@@ -27,6 +37,27 @@ export default {
   name: "employee-table",
   props: {
     employees: Array
+  },
+  data() {
+    return {
+      editing: null
+    };
+  },
+  methods: {
+    editMode(id) {
+      this.editing = id;
+    },
+
+    cancelEdit(employee) {
+      Object.assign(employee, this.cachedEmployee);
+      this.editing = null;
+    },
+
+    editEmployee(employee) {
+      if (employee.name === "" || employee.email === "") return;
+      this.$emit("edit:employee", employee.id, employee);
+      this.editing = null;
+    }
   }
 };
 </script>
